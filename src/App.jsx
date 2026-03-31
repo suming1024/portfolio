929879851c14863d79f7b184135c20af0f863c5e
soomin Jung <soominjung@soominui-MacBookPro.local>
2026-03-31 09:44:49 +0900

    변경사항

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ function App() {
 
   return (
     <>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<h1>Home</h1>} />
+          <Route path='/about' element={<h1>About</h1>} />
+          <Route path='/contact' element={<h1>Contact</h1>} />
+        </Routes>
+      </BrowserRouter>      
     </>
   )
 }
