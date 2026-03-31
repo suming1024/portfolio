e62ee1e14b0ba6585486a69fee83c85cbac5b5d3
soomin Jung <soominjung@soominui-MacBookPro.local>
2026-03-31 11:36:06 +0900

    vercel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import Main from '../layout/Main';
+// import Main from '../layout/Main';
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <h1>Home</h1>
+//       <p>This is the Home page.</p>
+//       <Main/>
+//     </div>
+//   );
+// }
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>This is the Home page.</p>
-      <Main/>
-    </div>
-  );
+  return <h1>홈이다</h1>
 }
