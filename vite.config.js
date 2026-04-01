2fc227751051d299b4f644910d6b236d741a2513
soomin Jung <soominjung@soominui-MacBookPro.local>
2026-04-01 11:51:08 +0900

    fix: build and router

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/portfolio/',
+//   server: {
+//     port: 3000,
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
-  server: {
-    port: 3000,
-  }
+  base: '/',  
 })
